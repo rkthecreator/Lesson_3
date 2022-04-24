9828b596ccafd4903cb2f5c60e9db61820c6fe01
RK <rkiper8@gmail.com>
2022-04-24 13:37:53 +0300

    Initial code Изначальный код

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public class Programm
+    {
+        public static void Main()
+        {
+            var value = 10;
+            value *= 2;
+            value /= 5;
+            value += 234;
+
+            var counter = value;
+            counter--;
+
+            Console.WriteLine("wow " + value);
+            Console.WriteLine("Counter: " + counter);
+            Console.ReadLine();
+        }
+    }
+}
